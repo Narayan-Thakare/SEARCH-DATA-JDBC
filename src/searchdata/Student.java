d90eefa974a585abdc5e3ead82e8dff83eec92f0
Narayan Thakare <narayan111thakare@gmail.com>
2023-06-03 21:19:42 +0530

    first commit

---
+++ src/searchdata/Student.java
@@ -0,0 +1,48 @@
+package searchdata;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+
+public class Student {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		System.out.println("narayan");
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("done");
+		System.out.println("done");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Studentt", "root", "abc123");
+		Statement st = con.createStatement();
+		try {
+			boolean found = false;
+			Scanner s=new Scanner(System.in);
+
+			System.out.print("Enter the roll no to search: ");
+			int searchroll = Integer.parseInt(s.nextLine());
+			
+			ResultSet rs = st.executeQuery("SELECT * FROM stu WHERE id = " + searchroll + "");
+			while (rs.next()) {
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+				found = true;
+			}
+			if (!found) {
+				System.out.println("Student not found");
+			}
+
+		} catch (Exception p) {
+			System.out.println("Enter the no. :" + p);
+
+		}
+		
+		
+		
+		
+		
+	}
+
+}
